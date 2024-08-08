79698359519447a1a6b98b0d97f24f9affbef021
Juanqx <antony556_04@outlook.com>
2024-08-08 12:46:44 -0600

    responsive Projects Home and added medias queries personalization

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,26 @@ export default {
     fontFamily: {
       syne: ["Syne Variable"],
     },
+    screens: {
+      xxsm: "443px",
+      xsm: "530px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      mmd: "880px",
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
